fdbbb942aff302a893b808c47f4d145195f1b5d5
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-06-14 15:09:17 -0500

    Added functions to get information about videos, they haven't been tested because API KEY has a problem

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,13 +1,119 @@
-# frozen_string_literal: true
+# frozen_string_literal => true
+require 'uri'
+require 'net/http'
+require 'openssl'
 
 # Retrieves information about videos from API
+# The object has the following format:
+#   {
+#     '_id' => 'string',
+#     'created_at' => 'string',
+#     'deleted_at' => 'string',
+#     'updated_at' => 'string',
+#     'on_air' => true,
+#     'purchase_price' => 0,
+#     'purchase_required' => true,
+#     'rating' => 0,
+#     'related_playlist_ids' => [],
+#     'rental_duration' => 0,
+#     'rental_price' => 0,
+#     'rental_required' => true,
+#     'request_count' => 0,
+#     'site_id' => 'string',
+#     'status' => 'string',
+#     'crunchyroll_id' => 'string',
+#     'hulu_id' => 'string',
+#     'mrss_id' => 'string',
+#     'kaltura_id' => 'string',
+#     'vimeo_id' => 'string',
+#     'youtube_id' => 'string',
+#     'thumbnails' => [],
+#     'transcoded' => true,
+#     'video_zobjects' => [],
+#     'active' => true,
+#     'discovery_url' => 'string',
+#     'custom_thumbnail_url' => 'string',
+#     'subscription_ads_enabled' => true,
+#     'title' => 'string',
+#     'zobject_ids' => [],
+#     'country' => 'string',
+#     'description' => 'string',
+#     'short_description' => 'string',
+#     'disable_at' => 'string',
+#     'enable_at' => 'string',
+#     'episode' => 0,
+#     'season' => 0,
+#     'featured' => true,
+#     'friendly_title' => 'string',
+#     'keywords' => [],
+#     'marketplace_ids' => {},
+#     'preview_ids' => [],
+#     'mature_content' => true,
+#     'pass_required' => true,
+#     'published_at' => 'string',
+#     'subscription_required' => true,
+#     'registration_required' => true,
+#     'custom_attributes' => {},
+#     'categories_attributes' => [],
+#     'segments' => [],
+#     'images_attributes' => [],
+#     'content_rules' => [],
+#     'manifest' => 'string'
+#   }
 module VideosHelper
+  PLAYER_KEY = Rails.application.credentials[:player_key]
+  API_KEY = Rails.application.credentials[:app_key]
+
+  # Returns a list of video objects
   def list
-    # to do
+    url = URI("https://api.zype.com/videos?api_key=#{API_KEY}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request['Accept'] = 'application/json'
+
+    response = http.request(request)
+    return [] if response.code != '200'
+
+    response.read_body
   end
 
+  # Returns a single video object based on its ID
   def details(id)
-    # to do
-    Rails.logger(id)
+    url = URI("https://api.zype.com/videos/#{id}?api_key=#{API_KEY}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request['Accept'] = 'application/json'
+
+    response = http.request(request)
+    return {} if response != '200'
+
+    response.read_body
+  end
+
+  # Renders iframe tag with video player
+  def player(video, access_token = '')
+    premium = video['subscription_required']
+    query_string = premium ? "access_token=#{access_token}" : "api_key=#{PLAYER_KEY}"
+    tag :iframe, src: "https://player.zype.com/embed/#{video['_id']}.html?#{query_string}"
+  end
+
+  # Validating if a consumer is entitled to play a video
+  def is_entitled?(video, access_token)
+    url = URI("https://api.zype.com/videos/#{video['_id']}/entitled?access_token=#{access_token}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request['Accept'] = 'application/json'
+
+    response = http.request(request)
+    response.read_body
   end
 end
--- spec/helpers/videos_helper_spec.rb
+++ spec/helpers/videos_helper_spec.rb
@@ -13,5 +13,43 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe VideosHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'list videos' do
+    it 'retrieves the list of videos' do
+      # Here I should mock the API, but this time I won't
+      list = helper.list
+      expect(list.is_a?(Array)).to be_truthy
+      expect(list.first.is_a?(Hash)).to be_truthy
+      expect(list.first).to include('_id')
+    end
+  end
+
+  describe 'show details of a video' do
+    it 'retrieves the list of videos' do
+      # Here I should mock the API, but this time I won't
+      list = helper.list
+      expect(helper.details(list.first['_id']).is_a?(Hash)).to be_truthy
+    end
+  end
+
+  describe 'shows player' do
+    it 'renders iframe with video player' do
+      # Here I should mock the API, but this time I won't
+      list = helper.list
+      access_token = 'abc123'
+      video = list.first
+      player = helper.player(video, access_token)
+      expect(player.is_a?(String)).to be_truthy
+      expect(player).to include('https://player.zype.com/embed')
+
+      # If it's a premium video it includes access token
+      video['subscription_required'] = true
+      player = helper.player(video, access_token)
+      expect(player).to include("access_token=#{access_token}")
+
+      # If it isn't a premium video it includes apí_key
+      video['subscription_required'] = false
+      player = helper.player(video, access_token)
+      expect(player).to include('api_key=')
+    end
+  end
 end
